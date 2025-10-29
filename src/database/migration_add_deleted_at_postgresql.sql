17ef29c403fcaff0bf098699517d8e40ca14d4a7
Benjamin Pinto <benjaminpinto@gmail.com>
2025-10-28 23:20:46 -0400

    feat: simplify db schema

--- src/database/migration_add_deleted_at_postgresql.sql
+++
@@ -1,3 +0,0 @@
--- PostgreSQL migration to add soft delete support
-ALTER TABLE schedules ADD COLUMN IF NOT EXISTS deleted_at TIMESTAMP DEFAULT NULL;
-CREATE INDEX IF NOT EXISTS idx_schedules_deleted_at ON schedules(deleted_at);

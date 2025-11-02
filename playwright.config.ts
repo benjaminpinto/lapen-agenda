a683cf057356c97789845c9195a6ec9d38ad669c
Benjamin Pinto <benjaminpinto@gmail.com>
2025-11-02 16:40:29 -0500

    fix: update CI to accommodate E2E tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     ['json', { outputFile: 'test-results/results.json' }]
   ],
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
